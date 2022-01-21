7d253125b5d643a03ebd007687c0337cdc7042bf
Robson-Brasil <robson.brasil@outlook.com>
2022-01-21 01:33:08 -0400

    Atualização

--- Livros/Linguagens de Programação/Ricardoduarte Python Para Desenvolvedores/Capitulo8/calc.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: latin1 -*-
-# Função definida no módulo
-def media(lista):
-
-    return float(sum(lista)) / len(lista)
--- Sketchs/Alarme_de_Distancia/Debug/VM_DBG/VM_mem_check_sam.cpp.d
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-C:\Users\Robson\ Brasil\AppData\Local\Temp\VMBuilds\Alarme_de_Distancia\uno\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
- c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\1tuiup1i.zbk\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
-=======
-C:\Users\Robson\ Brasil\AppData\Local\Temp\VMBuilds\Alarme_de_Distancia\uno\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
- c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\1tuiup1i.zbk\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
->>>>>>> a999f467d0d8ec227170bef607b275053355a84a
